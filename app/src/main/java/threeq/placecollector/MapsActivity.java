9566f34a75153f0f0d6fe5ec1201832caf6b5a6c
wtrr59 <wtrr59@naver.com>
2018-09-29 18:06:02 +0900

    seoul

--- app/src/main/java/threeq/placecollector/MapsActivity.java
+++ app/src/main/java/threeq/placecollector/MapsActivity.java
@@ -39,8 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng seoul = new LatLng(37.56, 126.97);
+
+        mMap.addMarker(new MarkerOptions().position(seoul).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
     }
 }
